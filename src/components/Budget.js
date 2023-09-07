31774c72039c9142dfdd52cab4632e6ab30b5c4b
ellenassis <ellenassis8@gmail.com>
2023-09-07 14:58:55 -0300

    budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,13 +3,27 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const { remaining } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const { expenses } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const inputValue = event.target.value;
+        if(inputValue <= 20000 && inputValue >= totalExpenses) {
+            setNewBudget(inputValue);
+        } else if(inputValue >= 20000) {
+            alert(`The value cannot exceed remaining funds £${remaining}`)
+        } else {
+            alert('You cannot reduce the budget value lower than the spending')
+        }
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
